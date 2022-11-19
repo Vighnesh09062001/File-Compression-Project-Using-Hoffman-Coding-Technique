e70dc9ebb60524ac73f94557c02a3bc19c04ab55
Vighnesh09062001 <vighneshgadge05@gmail.com>
2022-11-19 15:05:47 +0530

    first commit

---
+++ src/FileCompressor/QueueLL.java
@@ -0,0 +1,25 @@
+package FileCompressor;
+
+public class QueueLL<T> {
+    private SinglyLinkedList<T> list;
+
+    QueueLL() {
+        list = new SinglyLinkedList<>();
+    }
+
+    public boolean isEmpty() { return list.isEmpty(); }
+    public int size() { return list.size(); }
+
+    public void offer(T elem) {
+        list.addLast(elem);
+    }
+
+    public T poll() {
+        return list.removeFirst();
+    }
+
+    public T peek() {
+        return list.getHeadData();
+    }
+}
+
